ba2546431874e4860bc6109586fcc1f1857378dc
Danny Rottstegge <danny@rottstegge.net>
2020-05-18 20:11:07 +0200

    First in habemus-backend

---
+++ habemus-backend/habemus-webapp/src/test/java/de/rottstegge/habemus/HabemusWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rottstegge.habemus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabemusWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
