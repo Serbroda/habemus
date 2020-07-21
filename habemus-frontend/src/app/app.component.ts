387d28eda6b86fd7627be9921c0d12cbebf584fe
Danny Rottstegge <danny@rottstegge.net>
2020-07-21 14:51:56 +0200

    Apply .prettier in project for intellij

--- habemus-frontend/src/app/app.component.ts
+++ habemus-frontend/src/app/app.component.ts
@@ -3,24 +3,24 @@ import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { MatDrawerMode } from '@angular/material/sidenav';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'habemus-frontend';
-  mode: MatDrawerMode = 'side';
-  opened: boolean = true;
+    title = 'habemus-frontend';
+    mode: MatDrawerMode = 'side';
+    opened: boolean = true;
 
-  constructor(private breakpointObserver: BreakpointObserver) {
-    this.breakpointObserver.observe(['(max-width: 769px)']).subscribe((state: BreakpointState) => {
-      if (state.matches) {
-        this.mode = 'over';
-        this.opened = false;
-      } else {
-        this.mode = 'side';
-        this.opened = true;
-      }
-    });
-  }
+    constructor(private breakpointObserver: BreakpointObserver) {
+        this.breakpointObserver.observe(['(max-width: 769px)']).subscribe((state: BreakpointState) => {
+            if (state.matches) {
+                this.mode = 'over';
+                this.opened = false;
+            } else {
+                this.mode = 'side';
+                this.opened = true;
+            }
+        });
+    }
 }
