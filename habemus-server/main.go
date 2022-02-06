712dd7c774af467ac7a79b09fe1220cdb55d23f7
Danny Rottstegge <danny@rottstegge.net>
2022-02-06 22:22:42 +0100

    Add simple controller

--- habemus-server/main.go
+++ habemus-server/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+
+	"github.com/Serbroda/habemus-server/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	flagPort = flag.Int("p", 8080, "Server binding port")
+)
 
 func main() {
-	fmt.Println("Hello habemus-server")
+	flag.Parse()
+
+	bindingAddress := fmt.Sprintf("127.0.0.1:%d", *flagPort)
+
+	r := gin.Default()
+	r.GET("/ping", controllers.Index)
+	r.Run(bindingAddress)
 }
